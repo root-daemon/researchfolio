218b27f2c862dc176757ffae4d2bcbf2ecd60657
Rahuletto <rahulmarban@gmail.com>
2024-10-08 22:36:17 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import yaml from '@rollup/plugin-yaml';
+
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [yaml()]
+    },
+
+  integrations: [tailwind(), react()]
+});
